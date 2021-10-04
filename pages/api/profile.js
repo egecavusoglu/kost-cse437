3c2d224c820c30fb82c55f0a649570f57fb9a099
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-10-03 22:10:29 -0500

    Add login, get profile endpoints, cookie and jwt endpoints.

---
+++ pages/api/profile.js
@@ -0,0 +1,27 @@
+import { authenticationMiddleware } from 'src/lib/jwt';
+import { runMiddleware } from 'src/lib/middleware';
+/**
+ * @endpoint /profile
+ *
+ * @get returns the profile information of the user if they have valid auth cookie.
+ */
+
+export default async function handler(req, res) {
+  try {
+    await runMiddleware(req, res, authenticationMiddleware);
+    if (req.method == 'GET') {
+      const { id } = req.user;
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      user.hashedPassword = undefined;
+      return res.status(200).json({ isSuccess: true, data: { user: user } });
+    }
+    res.status(404).json({ isSuccess: false, error: 'Route not found!' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ isSuccess: false, error: err });
+  }
+}
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,22 +1,27 @@
 import { prisma } from 'src/lib/prisma';
 import bcrypt from 'bcrypt';
+
 export default async function handler(req, res) {
   try {
     if (req.method == 'POST') {
-      const { firstName, lastName, email, password } = req.body;
-      const hashedPass = await bcrypt.hash(password, 10);
-      const user = await prisma.user.create({
-        data: {
-          email: email,
-          firstName: firstName,
-          lastName: lastName,
-          hashedPassword: hashedPass,
-        },
-      });
+      const user = await saveNewUser(req.body);
       return res.status(200).json({ isSuccess: true, data: user });
     }
   } catch (err) {
-    console.error(err);
-    res.status(400).json({ isSuccess: false, error: err });
+    res.status(400).json({ isSuccess: false, error: err.message });
   }
+}
+
+async function saveNewUser(body) {
+  const { firstName, lastName, email, password } = body;
+  const hashedPass = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      hashedPassword: hashedPass,
+    },
+  });
+  return user;
 }
