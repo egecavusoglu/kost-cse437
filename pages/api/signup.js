c0ccdbfde5c84e36af420f47563a620832d15404
Ege Cavusoglu <egecavusoglu99@gmail.com>
2021-10-03 19:50:52 -0500

    Salt password before saving to db.

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,16 +1,16 @@
 import { prisma } from 'src/lib/prisma';
-
+import bcrypt from 'bcrypt';
 export default async function handler(req, res) {
   try {
     if (req.method == 'POST') {
-      console.log(req.body);
       const { firstName, lastName, email, password } = req.body;
+      const hashedPass = await bcrypt.hash(password, 10);
       const user = await prisma.user.create({
         data: {
           email: email,
           firstName: firstName,
           lastName: lastName,
-          hashedPassword: password,
+          hashedPassword: hashedPass,
         },
       });
       return res.status(200).json({ isSuccess: true, data: user });
