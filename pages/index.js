ea113cfbc7d3e1ec1598156f66bad10b8f3fe1d7
Owen Zhang <owenzhang76@gmail.com>
2021-10-27 19:25:06 -0500

    fix code

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Navbar from 'src/components/navbar';
 import { useOrgs } from 'src/requests/organisation';
 
 /// Homepage dashboard where your organizations live in. 
-
+/// Each file in /pages is an actual page navigtable by /xxx in the url. 
 export default function Home() {
   const { orgs, loading, error } = useOrgs();
 
@@ -20,7 +20,10 @@ export default function Home() {
         gap={6}
         p={8}>
         <AddOrgItem />
-        {loading ? <OrgItemSkeleton /> : orgs?.map((org) => <OrgItem key={org.id} data={org} />)}
+        {
+          error ? <div>Error loading organizations</div> : 
+        (loading ? <OrgItemSkeleton /> : orgs?.map((org) => <OrgItem key={org.id} data={org} />))
+        }
       </Grid>
     </div>
   );
