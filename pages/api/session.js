298cff7c825cdb4ec32681b1a51510747461e639
egecavusoglu <egecavusoglu99@gmail.com>
2021-11-02 14:19:30 -0500

    Add route protection for unauthenticated users with getServerSideProps

---
+++ pages/api/session.js
@@ -0,0 +1,24 @@
+import { stringifyError } from 'src/lib/error';
+import { checkValidAuthCookie } from 'src/lib/jwt';
+import { runMiddleware } from 'src/lib/middleware';
+import { prisma } from 'src/lib/prisma';
+
+/**
+ * @endpoint /session
+ * @get Checks if user has a valid session from the auth-token cookie
+ */
+
+export default async function handler(req, res) {
+  try {
+    if (req.method == 'GET') {
+      const hasValidCookie = checkValidAuthCookie(req);
+      if (hasValidCookie) {
+        return res.status(200).json({ isSuccess: true, data: { isLoggedIn: true } });
+      }
+      return res.status(401).json({ isSuccess: true, data: { isLoggedIn: false } });
+    }
+    res.status(404).json({ isSuccess: false, error: 'Route not found!' });
+  } catch (err) {
+    res.status(400).json({ isSuccess: false, error: stringifyError(err) });
+  }
+}
