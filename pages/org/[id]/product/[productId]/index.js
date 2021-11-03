b3e1403caa97c23b41a865774f6ceb1ea9f07383
Eugene Song <eugenesong@wustl.edu>
2021-11-02 19:58:31 -0500

    yay i created a page

---
+++ pages/org/[id]/product/[productId]/index.js
@@ -0,0 +1,10 @@
+import Navbar from 'src/components/navbar';
+
+export default function ProductDetailsPage() {
+  return (
+    <div>
+      <Navbar />
+      eugene created this
+    </div>
+  );
+}
