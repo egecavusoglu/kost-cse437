27ec9d477d8e5d7dd88ee778043fdd6f3d8bd24c
Eugene Song <eugenesong@wustl.edu>
2021-11-29 13:24:17 -0600

    product page refinement

--- pages/org/[id]/product/[productId]/index.js
+++ pages/org/[id]/product/[productId]/index.js
@@ -4,20 +4,34 @@ import { useProducts, useProdDetails } from 'src/requests/products';
 import ProdSelector from 'src/components/prod-selector';
 import Link from 'src/components/link';
 import { useRouter } from 'next/router';
+import { formatDate } from 'src/lib/date';
 import ProductItem, { AddProductItem, ProductItemSkeleton } from 'src/components/product-item';
-import { Button, Text, Flex, Box, Grid, Heading, Wrap, WrapItem, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Flex,
+  Box,
+  Grid,
+  Heading,
+  Wrap,
+  WrapItem,
+  VStack,
+} from '@chakra-ui/react';
 
 export default function ProductDetailsPage({ data = {}, ...props }) {
   const router = useRouter();
   const productId = router.query.productId;
   const orgId = router.query.id;
   const { product, loading, error } = useProdDetails(productId, orgId);
-  console.log(product);
+  // console.log(product?.createdAt);
+  // console.log(formattedDate);
   return (
     <>
       <div>
         <Navbar />
-        <ProdSelector />
         <Box m={[2, 4]} p={[2, 4]}>
           <Flex mb={4} alignItems="center" justifyContent="space-between">
             <Heading color="secondary.600" as="h2" fontSize="2xl">
@@ -27,17 +41,21 @@ export default function ProductDetailsPage({ data = {}, ...props }) {
           <Box py={1} mb={6}>
             <Text>{product?.description}</Text>
           </Box>
-        </Box>
-        <Link to={`/org/${orgId}/product/${productId}`} fontWeight="semibold" mb={2}>
-          {/* {data.name} */}
-        </Link>
-        <Box maxH={24} w="full" overflow="hidden">
-          <Text fontSize="sm" textAlign="left" color="gray.700" noOfLines={4}>
-            {/* {data.description} */}
-          </Text>
+          <Box py={1} mb={6}>
+            <Text color="gray.600" fontWeight="medium">
+              Created
+            </Text>
+            <Text>{formatDate(product?.createdAt)}</Text>
+            <Text color="gray.600" fontWeight="medium">
+              Billing Period and Amount
+            </Text>
+            <Text>
+              {product?.billingPeriod} {product?.amount} {product?.currency}
+            </Text>
+          </Box>
         </Box>
 
-        {/* username, pw, website link, cost, notes(how to use) */}
+        {/* username, pw, website link, notes */}
       </div>
     </>
   );
